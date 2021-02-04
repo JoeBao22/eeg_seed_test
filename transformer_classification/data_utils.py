ae2b8d3fac7fb897b143c17c51606d62fd1ac9d8
Xiaoyi Bao <baoxiaoyi1999@gmail.com>
2021-02-04 10:26:22 +0800

    modify data_utils.py so that the task is subject-dependent

--- transformer_classification/data_utils.py
+++ transformer_classification/data_utils.py
@@ -13,7 +13,7 @@ def create_examples(args,
     labels += 1  # deal with offset (original labels: -1, 0, 1 => new labels: 0, 1, 2)
 
     train_arr, train_label, test_arr, test_label = train_test_split(
-        features, labels, cumulative, args.train_subjects, args.sample_per_input)
+        features, labels, cumulative, args.train_percentage, args.sample_per_input)
     
     train_tensor = torch.tensor(train_arr, dtype=torch.float)
     train_label = torch.tensor(train_label, dtype=torch.int64).squeeze_(1)
@@ -33,41 +33,35 @@ def load_mat(dirname):
     return feature_arr, label_arr, cumulative_arr
     
 
-def train_test_split(feature_arr, label_arr, cumulative_arr, train_subjects, group_by):
-    train_trials = train_subjects * 3 * 15
+def train_test_split(feature_arr, label_arr, cumulative_arr, train_percentage, group_by):
     seq_len = feature_arr.shape[1]
     train_feature = None
     train_label = None
-    for i in range(train_trials+1):
-        start_index = cumulative_arr[i]
-        end_index = cumulative_arr[i+1]
-        remainder = (end_index - start_index) % group_by
-        end_index -=  remainder  # truncate
-        num_of_groups = (end_index - start_index) // group_by
-        temp_arr = feature_arr[start_index: end_index].reshape(num_of_groups, group_by, seq_len)
-        temp_label = label_arr[start_index: end_index: group_by]
-        if train_feature is None:
-            train_feature = temp_arr
-            train_label = temp_label
-        else:
-            train_feature = np.vstack((train_feature, temp_arr))
-            train_label = np.vstack((train_label, temp_label))
-    # print(train_feature.shape)  # (18272, 5, 310)
-    # print(train_label.shape)  # (18272, 1) 
     test_feature = None
     test_label = None
-    for i in range(train_trials+1, len(cumulative_arr)-1):
-        start_index = cumulative_arr[i]
-        end_index = cumulative_arr[i+1]
-        remainder = (end_index - start_index) % group_by
-        end_index -=  remainder  # truncate
-        num_of_groups = (end_index - start_index) // group_by
-        temp_arr = feature_arr[start_index: end_index].reshape(num_of_groups, group_by, seq_len)
-        temp_label = label_arr[start_index: end_index: group_by]
-        if test_feature is None:
-            test_feature = temp_arr
-            test_label = temp_label
-        else:
-            test_feature = np.vstack((test_feature, temp_arr))
-            test_label = np.vstack((test_label, temp_label))
+    for person_index in range(15):
+        for day_index in range(3):
+            train_trial_num = int(train_percentage * 15)
+            for trial_index in range(15):
+                start_index = cumulative_arr[(person_index * 3 + day_index) * 15 + trial_index]
+                end_index = cumulative_arr[(person_index * 3 + day_index) * 15 + trial_index + 1]
+                remainder = (end_index - start_index) % group_by
+                end_index -=  remainder  # truncate
+                num_of_groups = (end_index - start_index) // group_by
+                temp_arr = feature_arr[start_index: end_index].reshape(num_of_groups, group_by, seq_len)
+                temp_label = label_arr[start_index: end_index: group_by]
+                if trial_index < train_trial_num:
+                    if train_feature is None:
+                        train_feature = temp_arr
+                        train_label = temp_label
+                    else:
+                        train_feature = np.vstack((train_feature, temp_arr))
+                        train_label = np.vstack((train_label, temp_label))
+                else:
+                    if test_feature is None:
+                        test_feature = temp_arr
+                        test_label = temp_label
+                    else:
+                        test_feature = np.vstack((test_feature, temp_arr))
+                        test_label = np.vstack((test_label, temp_label))
     return train_feature, train_label, test_feature, test_label
