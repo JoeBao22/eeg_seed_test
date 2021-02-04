d867497f2ccc908ad7afc1437fd99283bcca2f68
Xiaoyi Bao <baoxiaoyi1999@gmail.com>
2021-02-04 09:30:58 +0800

    rename some parameters in resolving feature from dataset

--- dataset/read_mat_seed.py
+++ dataset/read_mat_seed.py
@@ -9,10 +9,9 @@ smooth_method_types = ["movingAve", "LDS"]
 # get labels:
 label_path = os.path.join(dir_path, "label.mat")
 labels = sio.loadmat(label_path)["label"][0]
-# print(labels)
 
 num_of_people = 15
-
+num_of_experiment = 15
 for feature_type in feature_types:
     for smooth_method_type in smooth_method_types:
         folder_name = os.path.join(dir_path, feature_type +"_" + smooth_method_type)
@@ -23,18 +22,18 @@ for feature_type in feature_types:
         stacked_label = None
         cumulative_samples = [0]
         for i in range(num_of_people):
-            print("resolving person {} / {}".format(i+1, len(labels)))
+            print("resolving person {} / {}".format(i+1, num_of_people))
             for trial_path in os.listdir(dir_path):
                 if trial_path.startswith(str(i+1) + "_"):  # trial record for the person
                     feature2dict= sio.loadmat(os.path.join(dir_path, trial_path))
-                    for label_index in range(len(labels)):
-                        k = feature_type + "_" + smooth_method_type + str(label_index+1)
+                    for experiment_index in range(num_of_experiment):
+                        k = feature_type + "_" + smooth_method_type + str(experiment_index+1)
                         v = feature2dict[k]
                         # print(v.shape)  # (62, 235, 5) for example
                         temp_arr = np.swapaxes(v, 0, 1).reshape(v.shape[1], -1)    
                         num_of_samples = temp_arr.shape[0]   
                         cumulative_samples.append(cumulative_samples[-1] + num_of_samples)
-                        temp_labels = np.ones((num_of_samples, 1)) * labels[label_index]          
+                        temp_labels = np.ones((num_of_samples, 1)) * labels[experiment_index]          
                         # print(temp_arr.shape)  # (235, 310) for example
                         if stacked_arr is None:
                             stacked_arr = temp_arr.copy()
@@ -45,6 +44,7 @@ for feature_type in feature_types:
         print("feature shape:", stacked_arr.shape)
         print("label shape: ", stacked_label.shape)
         cumulative_sample_arr = np.array(cumulative_samples)
+        print("cumulative sample shape: ", cumulative_sample_arr.shape)
         feature_path = os.path.join(folder_name, "feature.npy")
         label_path = os.path.join(folder_name, "label.npy")
         cumulative_samples_path = os.path.join(folder_name, "cumulative.npy")
